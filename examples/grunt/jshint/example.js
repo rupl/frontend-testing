23146dd4263f99f66e784a316bbfecadbc30f9fd
Chris Ruppel <chris.ruppel@gmail.com>
2013-09-25 14:40:57 +0200

    Adding more errors to example.js for jshint to find.

--- examples/grunt/jshint/example.js
+++ examples/grunt/jshint/example.js
@@ -1,3 +1,12 @@
-// This file has a syntax error that will trigger feedback from JSHint
+// This file has syntax errors that will trigger feedback from JSHint
 
 $('.my-sample-selector').hide()
+
+if (my_var == false) {
+  console.log('hello!');
+}
+
+var es3_object_broken = {
+  key1: 'value1',
+  key2: 'value2',
+}
