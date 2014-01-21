8bf9e9af4c301f31c7241260dc8f4e7b28ff826b
Chris Ruppel <chris.ruppel@gmail.com>
2014-01-20 23:58:37 -0600

    Adding Phantomas slides and grunt-phantomas example code.

---
+++ examples/grunt/phantomas/Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    phantomas: {
+      default: {
+        options: {
+          indexPath: './phantomas/',
+          options: {},
+          url: 'http://gruntjs.com/'
+        }
+      },
+      screenshot: {
+        options: {
+          indexPath: './phantomas/',
+          options: {
+            'screenshot': 'screenshots/sample-' + Date.now() + '.png'
+          },
+          url: 'http://gruntjs.com/'
+        }
+      },
+      requests: {
+        options: {
+          indexPath: './phantomas/',
+          options: {
+            'assert-requests': 20
+          },
+          url: 'http://gruntjs.com/'
+        }
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-phantomas');
+
+  grunt.registerTask('default', ['phantomas']);
+};
