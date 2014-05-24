d47743b24adafb289b2d1139fe761494fffec84c
Chris Ruppel <chris.ruppel@gmail.com>
2014-05-24 21:39:28 +0200

    Update CasperJS test for Picturefill
    Tests only checks for filename instead of reading the entire path
    of the <img> src now. Added clearer inline documentation for each
    step of the test.

--- examples/casper/picturefill.js
+++ examples/casper/picturefill.js
@@ -20,32 +20,43 @@ casper.test.begin('Testing Picturefill', 5, function suite(test) {
     // Now verify that the <picture> tag has three <source> tags
     test.assertElementCount('picture > source', 3);
 
-    // Now look for the value of the <img> and make sure it is set correctly.
-    // In this example we are testing the smaller viewport first, so medium.jpg
-    // should be found since the Picturefill markup declares it as default.
-    test.assertEvalEquals(function () {
-        return document.querySelectorAll("picture img")[0].getAttribute('src');
-    }, '../examples/images/medium.jpg', 'medium.jpg found using 320x480 viewport.');
+    // Before running any viewport-specific tests, set the viewport to 320x480.
+    // PhantomJS has a default viewport of 400x300 and CasperJS does not alter
+    // this default, so it's always a good idea to explicitly set the viewport
+    // to your desired dimensions.
+    this.viewport(320, 480);
   });
 
-  // casper.then allows us to wait until previous tests and actions are
+  // casper.then() allows us to wait until previous tests and actions are
   // completed before moving on to the next steps. This is useful for many
   // situations and viewport resizing is one of them, since scripts like
   // Picturefill have to respond to the resize.
+  //
+  // With the viewport resized, look for the src of the <img> and make sure it
+  // is set to the value we expect. In this example we are testing the smaller
+  // viewport first, so medium.jpg should be found since the Picturefill markup
+  // declares it as default.
+  casper.then(function() {
+    test.assertEvalEquals(function () {
+        return document.querySelectorAll("picture img")[0].getAttribute('src').match('medium.jpg').toString();
+    }, 'medium.jpg', 'medium.jpg found using 320x480 viewport.');
+  });
+
+  // Resize the viewport again
   casper.then(function() {
     this.viewport(960, 640);
   });
 
   // With the viewport resized to 960px, check and see if the large.jpg is
-  // now contained within the <img> src, since the demo code specifies an
+  // now contained within the <img> src, since the demo markup specifies an
   // 800px breakpoint for this image.
   casper.then(function() {
     test.assertEvalEquals(function () {
-        return document.querySelectorAll("picture img")[0].getAttribute('src');
-    }, '../examples/images/large.jpg', 'large.jpg found using 960x640 viewport.');
+        return document.querySelectorAll("picture img")[0].getAttribute('src').match('large.jpg').toString();
+    }, 'large.jpg', 'large.jpg found using 960x640 viewport.');
   });
 
-  // Resize the browser again
+  // Resize the viewport again
   casper.then(function() {
     this.viewport(1280, 1024);
   });
@@ -55,8 +66,8 @@ casper.test.begin('Testing Picturefill', 5, function suite(test) {
   // than 1000px wide.
   casper.then(function() {
     test.assertEvalEquals(function () {
-        return document.querySelectorAll("picture img")[0].getAttribute('src');
-    }, '../examples/images/extralarge.jpg', 'extralarge.jpg found using 1280x1024 viewport.');
+        return document.querySelectorAll("picture img")[0].getAttribute('src').match('extralarge.jpg').toString();
+    }, 'extralarge.jpg', 'extralarge.jpg found using 1280x1024 viewport.');
   });
 
   // This code runs all the tests that we defined above.
