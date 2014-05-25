011a7b1d56b0ddad5fa27f11e7b6e544a1c6af3c
Chris Ruppel <chris.ruppel@gmail.com>
2014-05-24 19:52:31 -0500

    Adding API reference links to my inline docs

--- examples/casper/picturefill.js
+++ examples/casper/picturefill.js
@@ -8,15 +8,21 @@
 // - Title, which shows up before any of the pass/fails.
 // - Number of tests, must be changed as you add tests.
 // - suite(), which contains all of your tests.
+//
+// @see http://casperjs.readthedocs.org/en/latest/modules/tester.html#begin
 casper.test.begin('Testing Picturefill', 5, function suite(test) {
 
   // casper.start() always wraps your first action. The first argument should
   // be the URL of the page you want to test.
+  //
+  // @see http://casperjs.readthedocs.org/en/latest/modules/casper.html#start
   casper.start('http://scottjehl.github.io/picturefill/examples/demo-02.html', function () {
 
     // First, we look for a <picture> element. The first argument is a query
     // selector, like jQuery or document.querySelectorAll(). Any <picture> tags
     // on the page are found by using the 'picture' selector.
+    //
+    // @see http://casperjs.readthedocs.org/en/latest/modules/tester.html#assertexists
     test.assertExists('picture', '<picture> element found.');
 
     // Now verify that the <picture> tag has three <source> tags. This is
@@ -24,12 +30,16 @@ casper.test.begin('Testing Picturefill', 5, function suite(test) {
     // a more specific selector. This will find all <source> tags within all
     // <picture> tags in your document. This demo only has one <picture> tag
     // so the simple selector works fine.
+    //
+    // @see http://casperjs.readthedocs.org/en/latest/modules/tester.html#assertelementcount
     test.assertElementCount('picture > source', 3);
 
     // Before running any viewport-specific tests, set the viewport to 320x480.
     // PhantomJS has a default viewport of 400x300 and CasperJS does not alter
     // this default, so it's always a good idea to explicitly set the viewport
     // to your desired dimensions.
+    //
+    // @see http://casperjs.readthedocs.org/en/latest/modules/casper.html#viewport
     this.viewport(320, 480);
   });
 
@@ -38,11 +48,15 @@ casper.test.begin('Testing Picturefill', 5, function suite(test) {
   // situations and viewport resizing is one of them, since scripts like
   // Picturefill have to respond to the resize.
   //
-  // With the viewport resized, look for the src of the <img> and make sure it
-  // is set to the value we expect. In this example we are testing the smaller
-  // viewport first, so medium.jpg should be found since the Picturefill markup
-  // declares it as default.
+  // @see http://casperjs.readthedocs.org/en/latest/modules/casper.html#then
   casper.then(function() {
+
+    // With the viewport resized, look for the src of the <img> and make sure it
+    // is set to the value we expect. In this example we are testing the smaller
+    // viewport first, so medium.jpg should be found since the Picturefill markup
+    // declares it as default.
+    //
+    // @see http://casperjs.readthedocs.org/en/latest/modules/tester.html#assertevalequals
     test.assertEvalEquals(function () {
         return document.querySelectorAll('picture img')[0].getAttribute('src').match('medium.jpg').toString();
     }, 'medium.jpg', 'medium.jpg found using 320x480 viewport.');
@@ -68,7 +82,7 @@ casper.test.begin('Testing Picturefill', 5, function suite(test) {
   });
 
   // Finally, with the 1280x1024 viewport, check to see if the <img> is now
-  // using the extralarge.jps source which was specified for viewports larger
+  // using the extralarge.jpg source which was specified for viewports larger
   // than 1000px wide.
   casper.then(function() {
     test.assertEvalEquals(function () {
