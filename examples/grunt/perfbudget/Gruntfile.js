99d40d0130d243138c460eb8efa65add87f6a0b4
Chris Ruppel <chris.ruppel@gmail.com>
2014-05-28 19:08:50 -0500

    Adding grunt-perfbudget example

---
+++ examples/grunt/perfbudget/Gruntfile.js
@@ -0,0 +1,38 @@
+// The settings var includes your WebPageTest API Key. To use yours, first go to
+// the following URL and follow the instructions to get a key:
+//
+// @see http://www.webpagetest.org/forums/showthread.php?tid=466
+//
+// Now make a file called settings.json in the same dir as this
+// Gruntfile and include an object like this:
+//
+// {
+//    "key": "your-web-page-test-api-key"
+// }
+var settings = require('./settings.json');
+
+// This is the normal Gruntfile
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    perfbudget: {
+      default: {
+        // See all options here:
+        // https://github.com/tkadlec/grunt-perfbudget#options
+        options: {
+          url: 'http://gruntjs.com',
+          key: settings.key,
+          budget: {
+            render: 1500,
+            requests: 32
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-perfbudget');
+
+  grunt.registerTask('default', ['perfbudget']);
+};
