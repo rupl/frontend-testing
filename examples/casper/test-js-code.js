03a76a09567e8de730852bfdd30c870501e01266
Chris Ruppel <chris.ruppel@gmail.com>
2018-04-11 11:42:55 +0200

    :white_check_mark: update Casper tests for JS code

--- examples/casper/test-js-code.js
+++ examples/casper/test-js-code.js
@@ -13,13 +13,13 @@ casper.test.begin('Basic code tests for jQuery and Modernizr', 3, function suite
   // You can output comments at any point in your script. Sometimes it's good
   // to add a message when you're attempting an operation that might take a
   // few moments to respond, such as this page load.
-  test.comment('⌚️  Opening http://fourword.fourkitchens.com...');
+  test.comment('⌚️  Opening https://rupl.github.io/frontend-testing/examples/targets/test-js-code.html');
 
   // casper.start() always wraps your first action. The first argument should
   // be the URL of the page you want to test.
   //
   // @see http://casperjs.readthedocs.org/en/latest/modules/casper.html#start
-  casper.start('http://fourword.fourkitchens.com', function () {
+  casper.start('https://rupl.github.io/frontend-testing/examples/targets/test-js-code.html', function () {
 
     // Look at a specific property on the jQuery object to check its version.
     //
@@ -30,23 +30,25 @@ casper.test.begin('Basic code tests for jQuery and Modernizr', 3, function suite
     //
     // @see http://casperjs.readthedocs.org/en/latest/modules/tester.html#assertevalequals
     test.assertEvalEquals(function () {
-      return jQuery.fn.jquery;
-    }, '1.7.1', 'jQuery 1.7.1 was found.');
+      // In jQuery 3+ the version number is accompanied by some build info, so
+      // our return value splits that extra data off.
+      return jQuery.fn.jquery.split(' ')[0];
+    }, '3.3.1', 'jQuery 3.3.1 was found.');
 
     // Look at a specific property on the Modernizr object to check its version.
     test.assertEvalEquals(function () {
       return Modernizr._version;
-    }, '2.6.2', 'Modernizr 2.6.2 was found.');
+    }, '3.6.0', 'Modernizr 3.6.0 was found.');
 
     // Check for required Modernizr tests. This is NOT testing the output! We
     // are only testing whether Modernizr is working, not what its output is.
     test.assertEvalEquals(function () {
       // An array of strings. Each one represents a required test that must be
       // found within Modernizr. That value might be true or false, but as long
-      // as it is boolean we ultimately do not care.
+      // as the type is Boolean we ultimately do not care.
       var requiredTests = [
-        'inlinesvg',
-        'svg'
+        'csstransformslevel2',
+        'serviceworker',
       ];
 
       // confirmedTests will end up as a single boolean. First, we map the
@@ -60,18 +62,18 @@ casper.test.begin('Basic code tests for jQuery and Modernizr', 3, function suite
       var confirmedTests = requiredTests.map(function(thisTest) {
         // This should return the type of data (boolean), not the boolean value.
         return typeof Modernizr[thisTest];
-      }).every(function(thisValue) {
+      }).every(function(thisType) {
         // Each data type will be verified to be boolean, and the result of the
         // .every() function will only be true if all the values resolved to true.
         // If even one of the values is not boolean, .every() returns false.
-        return thisValue === 'boolean';
+        return thisType === 'boolean';
       });
 
       // Return the result of our computation to Casper. The return value of
       // confirmedTests is ultimately what decides whether this test is reported
       // as PASS or FAIL. All of the other computation beforehand was internal.
       return confirmedTests;
-    }, true, 'Required Modernizr tests are all present. (inlinesvg, svg)');
+    }, true, 'Required Modernizr tests are all present.');
   });
 
   // This code runs all the tests that we defined above.
---
+++ examples/targets/README.md
@@ -0,0 +1,3 @@
+# Testing Targets for CasperJS
+
+This directory holds static assets that will be tested by CasperJS. If you're viewing this on GitHub, go one directory up and peer into the `casper` directory.
